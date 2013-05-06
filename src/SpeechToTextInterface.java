9889ca96f772fa01f4764813aa4139004c41e665
Jens Arvidsson <jensarv@gmail.com>
2013-05-06 10:53:16 +0200

    added image

--- src/SpeechToTextInterface.java
+++ src/SpeechToTextInterface.java
@@ -1,8 +1,11 @@
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import javaFlacEncoder.FLAC_FileEncoder;
 
@@ -12,6 +15,8 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.TargetDataLine;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -46,6 +51,7 @@ public class SpeechToTextInterface {
 		frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
 		frame.setSize (600, 800);
 		record = new JButton ("Record");
+		record.setIcon (getRecordingIcon ());
 		record.addActionListener (new ActionListener (){
 
 			@Override
@@ -64,15 +70,16 @@ public class SpeechToTextInterface {
 		});
 		resultArea = new JTextArea ();
 		JScrollPane scrollPane = new JScrollPane (resultArea);
-		scrollPane.setPreferredSize (new Dimension (500,500));
+		scrollPane.setPreferredSize (new Dimension (400,400));
 		JPanel p = new JPanel (new GridBagLayout ());
 		GridBagConstraints c = new GridBagConstraints ();
 		c.gridx = 0;
 		c.gridy = 0;
-		p.add (record, c);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		p.add (stopRecording, c);
 
 		c.gridx++;
-		p.add (stopRecording, c);
+		p.add (record, c);
 
 		c.gridy++;
 		c.gridx--;
@@ -160,6 +167,17 @@ public class SpeechToTextInterface {
 				e.printStackTrace();
 			}
 		}
+	}
+	
+	private Icon getRecordingIcon (){
+		BufferedImage img = new BufferedImage (16,16, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g = img.createGraphics ();
+		g.setColor (Color.RED);
+		g.fillOval (2, 2, 13, 13);
+		g.dispose ();
+		ImageIcon icon = new ImageIcon (img);
+		img.flush ();
+		return icon;
 	}
 
 }
